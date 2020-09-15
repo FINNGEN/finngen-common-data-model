d10d653bd84a7b376543fa4e1123a884e73e4bce
major seitan <majorseitan@gmail.com>
2020-09-15 02:48:50 +0000

    feat:import variant parsing
    Added support for parsing different variant formats.

--- test/test_genomics.py
+++ test/test_genomics.py
@@ -19,18 +19,39 @@ def test_locus_2():
     
 def test_variant_1():
     expected = Variant(chromosome = "1", position = 2, reference = "A", alternate = "G")
-    actual = Variant.from_str("chr1_2_A_G")
+    actual = Variant.from_str("1_2_A_G")
     assert expected == actual
                 
 
 def test_variant_2():
-    expected = "chr1_2_A_G"
-    actual = str(Variant.from_str(expected))
+    variant = "1_2_A_G"
+    expected = "1:2:A:G"
+    actual = str(Variant.from_str(variant))
     assert expected == actual
 
-def test_structural_variants():
-    expected = "chr9_96792507_T_<INS:ME:ALU>"
-    actual = str(Variant.from_str(expected))
+def test_variant_3():
+    variant = "1:2:A:G"
+    expected = "1:2:A:G"
+    actual = str(Variant.from_str(variant))
+    assert expected == actual
+
+def test_variant_3():
+    variant = "1_2_A/G"
+    expected = "1:2:A:G"
+    actual = str(Variant.from_str(variant))
+    assert expected == actual
+
+def test_structural_variants_1():
+    expected = "9:96792507:T:<INS:ME:ALU>"
+    variant = "chr9_96792507_T_<INS:ME:ALU>"
+    actual = str(Variant.from_str(variant))
     print(actual)
     assert expected == actual
     
+
+def test_structural_variants_2():
+    expected = "9:96792507:<INS:ME:ALU>:<INS:ME:ALU>"
+    variant = "chr9_96792507_<INS:ME:ALU>_<INS:ME:ALU>"
+    actual = str(Variant.from_str(variant))
+    print(actual)
+    assert expected == actual
