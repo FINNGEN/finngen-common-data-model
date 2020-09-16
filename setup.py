201c08b1417ac2bc373a329fe2800a16e57e8589
major seitan <majorseitan@gmail.com>
2020-09-16 00:03:26 +0000

    feat: setting up for github hosting

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 from distutils.core import setup
-setup(name='commons',
-      version='1.0',
-      description='data commons',
+setup(name='finngen-common-data-model',
+      version='0.0.1',
+      description='finngen common data model',
       author='major seitan',
-      packages=['commons'],
+      packages=['finngen-common-data-model'],
       install_requires=[ 'attrs>=19.3.0',
                          'SQLAlchemy>=1.3.18',
                          'pytest>=5.4.3' ])
